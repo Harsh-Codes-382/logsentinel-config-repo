806f7d6b791515e26a05e901a0548cb37e62d45c
Harsh-Codes-382 <harshkumar3826@gmail.com>
2026-02-01 21:59:24 +0530

    Configured the git repo in application.yml for config server

--- src/main/java/io/logsentinel/configserver/LogsentinelConfigServerApplication.java
+++ src/main/java/io/logsentinel/configserver/LogsentinelConfigServerApplication.java
@@ -1,4 +1,4 @@
-package io.logsentinel.logsentinel_config_server;
+package io.logsentinel.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
