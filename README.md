711c47b6482fa54b1f550f8d012de46f2612487e
Harsh-Codes-382 <harshkumar3826@gmail.com>
2026-02-01 21:37:44 +0530

    Initial commit logSentinel config server

---
+++ README.md
@@ -0,0 +1,1 @@
+# logsentinel-config-repo
---
+++ src/main/java/io/logsentinel/logsentinel_config_server/LogsentinelConfigServerApplication.java
@@ -0,0 +1,16 @@
+package io.logsentinel.logsentinel_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class LogsentinelConfigServerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(LogsentinelConfigServerApplication.class, args);
+    }
+
+}
